899787cc9671323382ae3293c1bdd98583343c07
Edge <edge27452696803@gmail.com>
2024-08-03 23:20:48 +0800

    SSM

---
+++ src/main/java/com/example/config/ServletConfig.java
@@ -0,0 +1,12 @@
+package com.example.config;
+
+/**
+ * @Description: servlet
+ * @ClassName: ServletConfig
+ * @Author: Edge
+ * @Date: 2024/8/3 23:20
+ * @Version: 1.0
+ */
+public class ServletConfig
+{
+}
