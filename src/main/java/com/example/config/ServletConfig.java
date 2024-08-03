afec3f2bc912778ffa7d586836989bb92afeb33b
Edge <edge27452696803@gmail.com>
2024-08-03 23:22:30 +0800

    SSM

--- src/main/java/com/example/config/ServletConfig.java
+++ src/main/java/com/example/config/ServletConfig.java
@@ -15,18 +15,18 @@ public class ServletConfig extends AbstractAnnotationConfigDispatcherServletInit
     @Override
     protected Class<?>[] getRootConfigClasses()
     {
-        return new Class[0];
+        return new Class[]{SpringConfig.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses()
     {
-        return new Class[0];
+        return new Class[]{SpringMvcConfig.class};
     }
 
     @Override
     protected String[] getServletMappings()
     {
-        return new String[0];
+        return new String[]{"/"};
     }
 }
