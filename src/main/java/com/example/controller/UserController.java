822bcbd7899439030d6ad4f5e44c806122f603d9
Edge <edge27452696803@gmail.com>
2024-08-03 23:33:50 +0800

    SSM

---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+/**
+ * @Description: controller
+ * @ClassName: UserController
+ * @Author: Edge
+ * @Date: 2024/8/3 23:33
+ * @Version: 1.0
+ */
+public class UserController
+{
+
+}
