4b12a2d51b87d28b187f05530d1baee3aa490ab0
Edge <edge27452696803@gmail.com>
2024-08-03 23:21:27 +0800

    SSM

--- src/main/java/com/example/config/ServletConfig.java
+++ src/main/java/com/example/config/ServletConfig.java
@@ -1,5 +1,7 @@
 package com.example.config;
 
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 /**
  * @Description: servlet
  * @ClassName: ServletConfig
@@ -7,6 +9,24 @@ package com.example.config;
  * @Date: 2024/8/3 23:20
  * @Version: 1.0
  */
-public class ServletConfig
+public class ServletConfig extends AbstractAnnotationConfigDispatcherServletInitializer
 {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses()
+    {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses()
+    {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings()
+    {
+        return new String[0];
+    }
 }
