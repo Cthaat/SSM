8296bbf5e9a8f1c6cc795047860389d516c92add
Edge <edge27452696803@gmail.com>
2024-08-03 23:03:22 +0800

    SSM

---
+++ src/main/java/com/example/config/JdbcConfid.java
@@ -0,0 +1,12 @@
+package com.example.config;
+
+/**
+ * @Description: jdbc
+ * @ClassName: JdbcConfid
+ * @Author: Edge
+ * @Date: 2024/8/3 23:02
+ * @Version: 1.0
+ */
+public class JdbcConfid
+{
+}
---
+++ src/main/java/com/example/config/MyBatisConfig.java
@@ -0,0 +1,12 @@
+package com.example.config;
+
+/**
+ * @Description: batis
+ * @ClassName: MyBatisConfig
+ * @Author: Edge
+ * @Date: 2024/8/3 23:03
+ * @Version: 1.0
+ */
+public class MyBatisConfig
+{
+}
