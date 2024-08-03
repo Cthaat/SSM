e28f1d319d0d655b73e2fc2488b6ab92488baa3a
Edge <edge27452696803@gmail.com>
2024-08-03 23:21:45 +0800

    SSM

---
+++ src/main/java/com/example/config/SpringMvcConfig.java
@@ -0,0 +1,12 @@
+package com.example.config;
+
+/**
+ * @Description: MVC
+ * @ClassName: SpringMvcConfig
+ * @Author: Edge
+ * @Date: 2024/8/3 23:21
+ * @Version: 1.0
+ */
+public class SpringMvcConfig
+{
+}
