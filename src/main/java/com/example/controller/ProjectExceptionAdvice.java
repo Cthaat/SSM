9399c5ceb134a555b28b4226142834bd5ea2821b
Edge <edge27452696803@gmail.com>
2024-08-04 15:06:10 +0800

    SSM

---
+++ src/main/java/com/example/controller/ProjectExceptionAdvice.java
@@ -0,0 +1,16 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @Description: excet
+ * @ClassName: ProjectExceptionAdvice
+ * @Author: Edge
+ * @Date: 2024/8/4 15:05
+ * @Version: 1.0
+ */
+
+@RestControllerAdvice
+public class ProjectExceptionAdvice
+{
+}
