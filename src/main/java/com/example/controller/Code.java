35cec5caced344a5d326bb3e8b0081ad98fc2a9d
Edge <edge27452696803@gmail.com>
2024-08-04 14:02:56 +0800

    SSM

---
+++ src/main/java/com/example/controller/Code.java
@@ -0,0 +1,12 @@
+package com.example.controller;
+
+/**
+ * @Description: code
+ * @ClassName: Code
+ * @Author: Edge
+ * @Date: 2024/8/4 14:02
+ * @Version: 1.0
+ */
+public class Code
+{
+}
---
+++ src/main/java/com/example/controller/Result.java
@@ -0,0 +1,64 @@
+package com.example.controller;
+
+/**
+ * @Description: result
+ * @ClassName: Result
+ * @Author: Edge
+ * @Date: 2024/8/4 14:01
+ * @Version: 1.0
+ */
+public class Result
+{
+    private Object data;
+    private Integer code;
+    private String message;
+
+    public Result()
+    {
+    }
+
+    public Result(Object data , Integer code)
+    {
+        this.data = data;
+        this.code = code;
+    }
+
+    public Result(Object data , Integer code , String message)
+    {
+        this.data = data;
+        this.code = code;
+        this.message = message;
+    }
+
+    public Object getData()
+    {
+        return data;
+    }
+
+    public void setData(Object data)
+    {
+        this.data = data;
+    }
+
+    public Integer getCode()
+    {
+        return code;
+    }
+
+    public void setCode(Integer code)
+    {
+        this.code = code;
+    }
+
+    public String getMessage()
+    {
+        return message;
+    }
+
+    public void setMessage(String message)
+    {
+        this.message = message;
+    }
+
+
+}
