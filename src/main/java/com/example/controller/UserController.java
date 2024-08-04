eb11a05d9c3f8d8833370eed2d5468aa6bce7562
Edge <edge27452696803@gmail.com>
2024-08-04 13:06:42 +0800

    SSM

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.example.controller;
 
 import com.example.domain.User;
+import com.example.service.UserService;
 import org.apache.ibatis.annotations.Insert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -12,7 +16,36 @@ import java.util.List;
  * @Date: 2024/8/3 23:33
  * @Version: 1.0
  */
+
+@RestController
+@RequestMapping ("/user")
 public class UserController
 {
+    @Autowired
+    private UserService userService;
 
+    public boolean save(User user)
+    {
+        return userService.save(user);
+    }
+
+    public boolean update(User user)
+    {
+        return userService.update(user);
+    }
+
+    public boolean delete(int id)
+    {
+        return userService.delete(id);
+    }
+
+    public User getById(int id)
+    {
+        return userService.getById(id);
+    }
+
+    public List<User> getAll()
+    {
+        return userService.getAll();
+    }
 }
